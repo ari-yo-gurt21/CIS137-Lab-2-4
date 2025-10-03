087bf5cb40e9638313ca406be88dbc8741e3e706
Ari Lee <Theboss21ari@gmail.com>
2025-10-03 14:33:42 -0700

    Initial Commit

---
+++ Lab2_4/Lab2_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2_4App.swift
+//  Lab2_4
+//
+//  Created by Ari Lee on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Lab2_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab2_4Tests/Lab2_4Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Lab2_4Tests.swift
+//  Lab2_4Tests
+//
+//  Created by Ari Lee on 10/3/25.
+//
+
+import Testing
+
+struct Lab2_4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
